7903bba92ce69a6194c0f1b54382e1506172c2eb
Mike Field <field1062000@yahoo.com>
2016-03-04 13:35:42 -0800

    Fixed #2. You can now toggle the follow button on king kong's timeline.

--- scripts/default.js
+++ scripts/default.js
@@ -38,7 +38,7 @@
       tweetCount: 2,
       profileImage: 'images/osakaflu.jpg',
       headerImage: 'images/yokohama_fuji.jpg',
-      activeUser: true
+      isActiveUser: true
     },
     {
       username: 'godzilla',
@@ -48,7 +48,7 @@
       tweetCount: 2,
       profileImage: 'images/godzilla profile.jpg',
       headerImage: 'images/godzilla header.png',
-      activeUser: false
+      isActiveUser: false
     },
     {
       username: 'kingkong',
@@ -58,26 +58,25 @@
       tweetCount: 2,
       profileImage: 'images/kingkong.jpg',
       headerImage: 'images/kongheader.jpg',
-      activeUser: false
+      isActiveUser: false
     }
   ];
 
-  var activeUser = 'kaijuking';
-  var inactiveUser = '';
+var activeUser = getActiveUser();
+//var inactiveUser = '';
 
   var tweet = function(username, message) {
     this.username = username;
     this.message = message;
   };
 
-
-/*Post A Tweet*/
+/*Setup process*/
 document.addEventListener('DOMContentLoaded', function(event){
   console.log('DOM has fully loaded and parsed.');
   displayTimeLineTweets();
   configureTimelineTweets();
-  setupActiveUserTimeline();
-  //displayUserTweets(activeUser);
+  console.log('Active User = ' + getActiveUser());
+  console.log(getAllFollowing(getActiveUser()));
 });
 
 var homeButton = document.getElementById('home-button');
@@ -92,46 +91,31 @@ myForm.addEventListener('submit', function(event){
   var message = document.getElementById('newTweet');
   var tweetMessage = message.value;
 
-  createTweet(activeUser, tweetMessage);
-  displayTweet(activeUser, tweetMessage, 'activeuser');
-  //updateTweetCount(activeUser);
+
+  var theTwitterTimeline = 'twitter-timeline';
+  var theActiveUserTimeline = 'activeuser-all-tweets';
+
+  user = getActiveUser();
+
+  createTweet(user, tweetMessage);
+  displaySingleTweet(user, tweetMessage) //, theTwitterTimeline);
+  //createTweet(user, tweetMessage);
+  //displaySingleTweet(user, tweetMessage, theActiveUserTimeline);
 
 }, false);
 
-var modalTweet = document.getElementById('modalTweet');
-modalTweet.addEventListener('submit', function(event) {
+var followButton = document.getElementById('follow-button');
+followButton.addEventListener('click', function(event) {
+  event.preventDefault();
+  var b = getAllFollowing('kaijuking');
+  console.log(b.length);
 
-  console.log('hhehehehe');
-
-  var message = document.getElementById('modal-newTweet');
-  var tweetMessage = message.value;
-
-  createTweet(activeUser, tweetMessage);
-  displayTweet(activeUser, tweetMessage, 'activeuser');
-
+  followButton.removeChild(followButton.firstChild);
+  var text = document.createTextNode('Following');
+  followButton.appendChild(text);
+  followButton.setAttribute('class', 'btn btn-info show');
 });
 
-
-/*Active User's Timeline - The following functions are used for Issue #3*/
-/*Populate the user's REALNAME, USERNAME, #FOLLOWING and #FOLLOWERS <span> elments on DOM load*/
-function setupActiveUserTimeline(){
-  var realNameText = getRealName(activeUser);
-  console.log(realNameText);
-  var realName = document.getElementById('activeuser-realname');
-  realName.textContent = realNameText;
-
-  var userNameText = '@' + getUserName(activeUser);
-  var userName = document.getElementById('activeuser-username');
-  userName.textContent = userNameText;
-
-  var followerCount = getTotalFollowers(activeUser);
-  var numFollowers = document.getElementById('activeuser-numFollowers')
-  numFollowers.textContent = followerCount + " Followers";
-
-  var followingCount = getTotalFollowing(activeUser);
-  var numFollowing = document.getElementById('activeuser-numFollowing')
-  numFollowing.textContent = followingCount + " Following";
-};
 
 /*Create a new tweet object and push it into the allTweets array*/
 function createTweet(username, tweetMessage) {
@@ -141,7 +125,7 @@ function createTweet(username, tweetMessage) {
 };
 
 /*Generate and append the elements needed to post a new tweet*/
-function displayTweet(username, tweetMessage, pNode) {
+function displaySingleTweet(username, tweetMessage){
 
   /*Create the element to hold the user's profile picture*/
   var newImage = document.createElement('img');
@@ -204,15 +188,7 @@ function displayTweet(username, tweetMessage, pNode) {
   newRowDiv.setAttribute('class', 'row');
   newRowDiv.appendChild(newColumnDiv);
 
-  if(pNode == 'activeuser') {
-    /*Append the ROW div, which holds the new tweet, to the parent element*/
-    var parentNode = document.getElementById(pNode + '-all-tweets');
-    parentNode.appendChild(newRowDiv);
-  }
-  if(pNode == 'inactiveuser') {
-    var parentNode = document.getElementById(pNode + '-all-tweets');
-    parentNode.appendChild(newRowDiv);
-  }
+  var parentNode = document.getElementById('twitter-timeline');
 
 };
 
@@ -294,36 +270,31 @@ function displayTimeLineTweets() {
 function configureTimelineTweets(){
   /*Shows either the 'follow-user' DIV (Issue #2) or 'post-tweet' DIV (Issue #3)*/
   var linkBtn = document.getElementsByTagName('a');
-  var inactiveUserDiv = document.getElementById('inactiveuser-timeline');
-  var activeUserDiv = document.getElementById('activeuser-timeline');
+  //var inactiveUserDiv = document.getElementById('inactiveuser-timeline');
+  //var activeUserDiv = document.getElementById('activeuser-timeline');
   //var twitterTimelineDiv = document.getElementById('twitter-timeline');
 
   for(var i = 0; i < linkBtn.length; i++){
     linkBtn[i].addEventListener('click', function(event) {
-      var theEvent = event.target.parentNode.name;
-      //console.log(theEvent);
-      var myEvent = event.target.parentNode;
-      //console.log(myEvent);
-      if(theEvent != null && theEvent != activeUser) {
-        inactiveUser = theEvent;
-        //console.log(activeUser);
-        //console.log(theEvent);
-        inactiveUserDiv.setAttribute('class', 'show');
-        activeUserDiv.setAttribute('class', 'hidden');
-        myEvent.setAttribute('href', '#follow-user');
-        setupInactiveUserTimeline(inactiveUser);
-        //displayUserTweets(inactiveUser);
-      };
-      if(theEvent != null && theEvent === activeUser) {
-        inactiveUserDiv.setAttribute('class', 'hidden');
-        activeUserDiv.setAttribute('class', 'show');
-        myEvent.setAttribute('href', '#post-tweet');
+      var targetUserName = event.target.parentNode.name;
+      console.log(targetUserName);
+      var userTimeline = document.getElementById('user-timeline');
+      var twitterTimeline = document.getElementById('twitter-timeline');
+
+      if(targetUserName != null){
+        console.log('targetUserName is not null');
+        displayAllUserTweets(targetUserName);
+        setupUserTimeline(targetUserName);
+        toggleTweetFollowButton(targetUserName);
+        twitterTimeline.setAttribute('class', 'container hidden');
+        userTimeline.setAttribute('class', 'container show');
+        console.log(getAllFollowers(targetUserName));
       };
     });
   };
 };
 
-function setupInactiveUserTimeline(username){
+function setupUserTimeline(username){
   var realNameText = getRealName(username);
   var realName = document.getElementById('inactiveuser-realname');
   realName.textContent = realNameText;
@@ -356,30 +327,102 @@ function setupInactiveUserTimeline(username){
   console.log("header image url = " + headerImageURL);
 };
 
-function displayUserTweets(username) {
-  console.log(username)
-  if(username === activeUser){
-    pNode = 'activeuser';
-  }
-  else {
-    pNode = 'inactiveuser';
-  }
+/*Generate and append the elements needed to post a new tweet*/
+function displayAllUserTweets(username) {
   for( var i = 0; i < allTweets.length; i++) {
     if(username === allTweets[i].username) {
-      console.log(allTweets[i].message);
-      displayTweet(username, allTweets[i].message, pNode);
+      var newImage = document.createElement('img');
+      newImage.setAttribute('class', 'media-object img-thumbnail img-responsive profile-image');
+      newImage.setAttribute('src', getProfileImageUrl(username));
+
+      var newImageLink = document.createElement('a');
+      newImageLink.setAttribute('href', '#');
+      newImageLink.setAttribute('name', getUserName(username));
+      newImageLink.appendChild(newImage);
+
+      var newImageMediaDiv = document.createElement('div');
+      newImageMediaDiv.setAttribute('class', 'media-left media-top');
+      newImageMediaDiv.appendChild(newImageLink);
+
+      var newColumnDiv = document.createElement('div'); //this will need to be appended to newRowDiv
+      newColumnDiv.setAttribute('class', 'col-md-12');
+      newColumnDiv.appendChild(newImageMediaDiv);
+
+      /*Create the elements to hold the user's realname, username, retweet/favorite icons and the tweet message*/
+      var favoriteIcon = document.createElement('i'); //this needs to be appended to spanFavoriteIcon
+      favoriteIcon.setAttribute('class', 'fa fa-heart-o');
+      var spanFavoriteIcon = document.createElement('span');
+      spanFavoriteIcon.setAttribute('class', 'tweet-favorite');
+      spanFavoriteIcon.appendChild(favoriteIcon);
+
+      var retweetIcon = document.createElement('i'); //this needs to be appended to spanRetweetIcon
+      retweetIcon.setAttribute('class', 'fa fa-retweet');
+      var spanRetweetIcon = document.createElement('span');
+      spanRetweetIcon.setAttribute('class', 'tweet-retweet');
+      spanRetweetIcon.appendChild(retweetIcon);
+
+      var spanTweetRealName = document.createElement('span');
+      spanTweetRealName.setAttribute('class', 'tweet-realname');
+      var realName = document.createTextNode(getRealName(username));
+      spanTweetRealName.appendChild(realName);
+
+      var spanTweetUserName = document.createElement('span');
+      spanTweetUserName.setAttribute('class', 'tweet-username');
+      var userName = document.createTextNode(getUserName(username));
+      spanTweetUserName.appendChild(userName);
+
+      var newMediaHeading = document.createElement('h4'); //this needs to be be appended to newMediaBodyDiv
+      newMediaHeading.setAttribute('class', 'media-heading');
+      newMediaHeading.appendChild(spanTweetUserName);
+      newMediaHeading.appendChild(spanTweetRealName);
+      newMediaHeading.appendChild(spanRetweetIcon);
+      newMediaHeading.appendChild(spanFavoriteIcon);
+
+      var newMediaBodyDiv = document.createElement('div'); //this needs to be appended to newRowDiv
+      newMediaBodyDiv.setAttribute('class', 'media-body');
+      newMediaBodyDiv.appendChild(newMediaHeading);
+      var theTweetMessage = document.createTextNode(allTweets[i].message);
+      var messageParagraph = document.createElement('p');
+      messageParagraph.appendChild(theTweetMessage);
+      newMediaBodyDiv.appendChild(messageParagraph);
+
+      newColumnDiv.appendChild(newMediaBodyDiv);
+      var newRowDiv = document.createElement('div'); //this will need to be appended to parentNode
+      newRowDiv.setAttribute('class', 'row');
+      newRowDiv.appendChild(newColumnDiv);
+
+      var parentNode = document.getElementById('inactiveuser-all-tweets');
+      parentNode.appendChild(newRowDiv);
     };
   };
 };
 
-
 /*--------------------------------------------------------------------------*/
 /*Return all the user's Tweets based upon the user's username*/
+function toggleTweetFollowButton(username){
+  var btnPost = document.getElementById('post-button');
+  var btnFollow = document.getElementById('follow-button');
+  var followingState = isFollowing(username);
+  for(var i = 0; i < allUsers.length; i++) {
+    var active = getActiveUser();
+    if(username === active) {
+      btnPost.setAttribute('class', 'btn btn-primary hide');
+      btnFollow.setAttribute('class', 'btn btn-primary hide');
+    } else if (username != active && followingState == true) {
+      btnPost.setAttribute('class', 'btn btn-primary hide');
+      btnFollow.setAttribute('class', 'btn btn-primary hide');
+    } else {
+      btnPost.setAttribute('class', 'btn btn-primary hide');
+      btnFollow.setAttribute('class', 'btn btn-primary show');
+    };
+  };
+};
+
 function getAllTweets(username) {
   for(var i = 0; i < allTweets.length; i++) {
     if(allTweets[i].username === username) {
-      //return allTweets[i].message;
-      console.log(allTweets[i].message);
+      return allTweets[i].message;
+      //console.log(allTweets[i].message);
     }
   }
 }
@@ -399,9 +442,27 @@ function getAllFollowing(username) {
   for(var i = 0; i < allUsers.length; i++) {
     if(allUsers[i].username === username) {
       return allUsers[i].following;
-      console.log(allUsers[i].following);
+      console.log(username + ' is following: ' + allUsers[i].following);
     };
   };
+};
+
+function isFollowing(username){
+  var isAFollower = false;
+  for(var i = 0; i < allUsers.length; i++){
+    if(allUsers[i].username === username){
+      for(var n = 0; n < allUsers[i].followers.length; n++){
+        console.log(allUsers[i].followers[n]);
+        if(allUsers[i].followers[n] === activeUser){
+          isAFollower = true;
+        } else {
+          continue;
+        };
+      };
+    };
+  };
+  console.log(isAFollower);
+  return isAFollower;
 };
 
 /*Add a new 'follower' to the user*/
@@ -458,7 +519,7 @@ function getProfileImageUrl(username) {
 function getHeaderImageUrl(username) {
   for(var i = 0; i < allUsers.length; i++) {
     if(allUsers[i].username === username) {
-      console.log(allUsers[i].headerImage);
+      //console.log(allUsers[i].headerImage);
       return allUsers[i].headerImage;
     };
   };
@@ -480,18 +541,10 @@ function getRealName(username) {
   }
 };
 
-function displayUserTweets(username) {
-  console.log(username)
-  if(username === activeUser){
-    pNode = 'activeuser';
-  }
-  else {
-    pNode = 'inactiveuser';
-  }
-  for( var i = 0; i < allTweets.length; i++) {
-    if(username === allTweets[i].username) {
-      console.log(allTweets[i].message);
-      displayTweet(username, allTweets[i].message, pNode);
+function getActiveUser() {
+  for(var i = 0; i < allUsers.length; i++) {
+    if(allUsers[i].isActiveUser == true){
+      return allUsers[i].username;
     };
   };
 };
